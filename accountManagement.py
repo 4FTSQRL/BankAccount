77f1f65477958aa3dd1ba975cc884b01b31b8892
4FTSQRL <156530576+4FTSQRL@users.noreply.github.com>
2024-04-25 17:06:26 -0400

    Create accountManagement.py

---
+++ accountManagement.py
@@ -0,0 +1,31 @@
+"""
+    Author: Elicia Ramitt
+    
+    Usage: python accountManagement.py
+    
+    Description: Different account management: logging in, delete account,
+"""
+
+# Import Statements
+import sqlite3
+import createAccounts
+
+# Main Function for Testing
+def main():
+    return 0
+
+# Log In Function
+def logIn(name, password):
+    """Allows for the user to log into their account. Confirms that their credentials are valid
+
+    Args:
+        name (str): the name for the account
+        password (str): the password for the account
+        
+    Returns:
+        balance (int): the balance attached to the account
+    """
+    
+# Python Incantation
+if __name__ == "__main__":
+    main()
