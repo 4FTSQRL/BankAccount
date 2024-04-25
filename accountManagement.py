573875a954d5627f7e53da93d0b0c8127f4ed589
4FTSQRL <156530576+4FTSQRL@users.noreply.github.com>
2024-04-25 17:21:11 -0400

    Update accountManagement.py
    Log in Function complete

--- accountManagement.py
+++ accountManagement.py
@@ -25,6 +25,35 @@ def logIn(name, password):
     Returns:
         balance (int): the balance attached to the account
     """
+    # Get the database path
+    databasePath = createAccounts.getFilePath()
+    # Create the connection
+    con = sqlite3.connect(databasePath)
+    # Cursor
+    cur = con.cursor()
+    # Get the password of the account holder
+    cur.execute("SELECT password FROM acconuts WHERE name = ?", (name,))
+    # Fetch the password
+    pswd = cur.fetchone()
+    # See if it is the same
+    if pswd == password:
+        # Print success statement
+        print("Login successful.")
+        # Get the balance
+        cur.execute("SELECT balance FROM accounts WHERE password = ?", (password,))
+        # Fetch thr balance
+        balance = cur.fetchone()
+        # Return the balance
+        return balance
+    # If the passwords are not the same, do not log in
+    else:
+        # Print failure.
+        print("Login unsuccessful.")
+        # Return None
+        return None
+    
+    # Close the connection
+    con.close()
     
 # Python Incantation
 if __name__ == "__main__":
